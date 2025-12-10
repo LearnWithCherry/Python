3a4c6382b929e5a0726fe3f4c2dc0ecdf866a0e4
Rajat Bhardwaj <rajatb1008@gmail.com>
2025-12-11 00:59:11 +0530

    Add files via upload

---
+++ Code With Harry/operators/Arithmetic.py
@@ -0,0 +1,22 @@
+a = 5
+b = 10
+
+print(a+b)           #  15
+print(a/b)           #  0.5
+print(a-b)           #  -5
+print(a*b)           #  50
+print(a//b)          #  0
+print(a%b)           #  5
+print(a**b)          #  9765625
+
+
+'''| Operator | Description         | Example          |
+| -------- | ------------------- | ---------------- |
+| `+`      | Addition            | `5 + 3` → `8`    |
+| `-`      | Subtraction         | `5 - 2` → `3`    |
+| `*`      | Multiplication      | `4 * 2` → `8`    |
+| `/`      | Division            | `10 / 2` → `5.0` |
+| `//`     | Floor Division      | `10 // 3` → `3`  |
+| `%`      | Modulus (remainder) | `10 % 3` → `1`   |
+| `**`     | Exponentiation      | `2 ** 3` → `8`   |
+'''
---
+++ Code With Harry/operators/logical.py
@@ -0,0 +1,6 @@
+'''| Operator | Description           | Example                  |
+| -------- | --------------------- | ------------------------ |
+| `and`    | True if both are True | `True and True` → `True` |
+| `or`     | True if one is True   | `True or False` → `True` |
+| `not`    | Reverses True/False   | `not True` → `False`     |
+'''
