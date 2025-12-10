62d2a2c75e703d60f4c1868e5d6fdf1eef7ed5c1
Rajat Bhardwaj <rajatb1008@gmail.com>
2025-12-11 01:00:38 +0530

    Add files via upload

---
+++ programs/user.py
@@ -0,0 +1,10 @@
+user = int(input("Enter User name: "))
+password = str(input("Enter User Password: "))
+
+if user == 1250 and password == "qwer":
+    print("Welcome User")
+elif user != 1250:
+    print("Invalid User.") 
+elif password != "qwer":
+    print("Invalid Password")
+ 
---
+++ programs/varible.py
@@ -0,0 +1,6 @@
+x = 10
+y = "Rajat"
+z = 102.0
+print(type(x))
+print(type(y))
+print(type(z))
