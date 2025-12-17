28afa785f6732516a3610dc7a192fe964852f291
Rajat Bhardwaj <rajatb1008@gmail.com>
2025-12-17 13:08:30 +0530

    Add files via upload

---
+++ programs/operator.py
@@ -0,0 +1,8 @@
+# operator 
+
+#    Type          Example              Meaning                   
+
+#    Arithmetic    + - * / % ** //    Math operations           
+#    Comparison    == != > < >= <=    Compare values            
+#    Logical       and or not         Combine conditions        
+#    Assignment    = += -= *=         Assign/update variables   
