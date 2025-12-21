d2d7bd5376225533e557186fc14b1ee5d6bdb6fe
Rajat Bhardwaj <rajatb1008@gmail.com>
2025-12-22 00:16:38 +0530

    Add files via upload

---
+++ Code With Harry/Chapter 5/set_methos.py
@@ -0,0 +1,13 @@
+s = {1,2,4,4,5,"rajat"} 
+
+print(s) # this will print elements of S
+
+print(len(s))
+# this will print the length of the set
+
+s.remove(5)
+print(s)
+# will remove 5(selected number) from the set and give a new output
+
+print(s.pop())
+
