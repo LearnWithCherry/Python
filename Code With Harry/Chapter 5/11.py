51615edaead59508bac94379b5a959872b527d99
Rajat Bhardwaj <rajatb1008@gmail.com>
2025-12-23 00:03:40 +0530

    Add files via upload

---
+++ Code With Harry/Chapter 5/11.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Cherry",
+    "age": 19,
+    "skills": ["Python", "ML"]
+}
+
+print(student["name"])       # Cherry
+student["age"] = 20          # update
+student["city"] = "Delhi"    # add
+del student["skills"]        # delete
