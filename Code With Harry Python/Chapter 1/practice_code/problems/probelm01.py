57afc7c2e7683cf38749467c632f5451bc66ed74
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-04 11:45:33 +0530

    Add files via upload

---
+++ Code With Harry Python/Chapter 1/practice_code/problems/probelm01.py
@@ -0,0 +1,3 @@
+print('''You are not behind. 
+      You are just building the foundation for something legendary. 
+      Keep going, because every small step forward is rewriting your story into greatness."''')
