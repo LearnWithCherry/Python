7c2f2961ef25e41680abc6b1e01d39d6bf004c1a
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-04 11:47:04 +0530

    Add files via upload

---
+++ Code With Harry Python/Chapter 2/operators/Comparision.py
@@ -0,0 +1,11 @@
+'''
+| Operator | Meaning               | Example           |
+| -------- | --------------------- | ----------------- |
+| `==`     | Equal to              | `5 == 5` → `True` |
+| `!=`     | Not equal to          | `5 != 3` → `True` |
+| `>`      | Greater than          | `5 > 3` → `True`  |
+| `<`      | Less than             | `5 < 7` → `True`  |
+| `>=`     | Greater than or equal | `5 >= 5` → `True` |
+| `<=`     | Less than or equal    | `4 <= 5` → `True` |
+'''
+
---
+++ Code With Harry Python/Chapter 2/operators/identify_&_membership.py
@@ -0,0 +1,12 @@
+'''| Operator | Example      | Meaning             |
+| -------- | ------------ | ------------------- |
+| `is`     | `x is y`     | True if same object |
+| `is not` | `x is not y` | True if not same    |
+'''
+
+'''7. 📦 Membership Operators
+Used to check if a value exists in a collection.
+
+Operator	Example	Meaning
+in	3 in [1,2,3]	True
+not in	5 not in [1,2]	True'''
