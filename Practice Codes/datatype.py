0cef34d9784d43973e9a9eac57fd87b8a737c3ed
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-10 16:07:12 +0530

    Add files via upload

---
+++ Practice Codes/datatype.py
@@ -0,0 +1,10 @@
+# data types in python 
+
+a = 10   # int 
+b = 10.2  # float 
+c = "Rajat"  # str
+bool = True  # bool
+mylist = [1,2,3]  # list
+mytuple = (1,2,3)  # tuples
+mydicy = {"Name": "Cherry"}  # dict
+myset = {1,2,3}  #  sets
