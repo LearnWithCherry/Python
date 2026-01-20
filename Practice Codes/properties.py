eb189a1f3f0ca94d36b7f328707b46bfed894c1a
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-20 23:49:24 +0530

    Add files via upload

---
+++ Practice Codes/properties.py
@@ -0,0 +1,16 @@
+letter = "Z" * 10
+
+print(letter)
+
+# string like "2" + "3" will not add
+# they will concatinate and prints like 23
+
+a = "rajat"
+
+print(a.find("r"))
+
+b = "Hi my name is rajat"
+
+print(b.split())
+# if i put any charaters in split() it will
+# remove from the list and print other
