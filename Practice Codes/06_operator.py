208ea1e98635e4cf3006c74453858fb224a6fde7
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-21 20:33:35 +0530

    Add files via upload

---
+++ Practice Codes/06_operator.py
@@ -0,0 +1,29 @@
+'''
+1. Arithmetic Operators
+
+    +   -   *   /   %   //   **
+
+2. Assignment Operators
+
+    =   +=   -=   *=   /=   %=   //=   **=   &=   |=   ^=   >>=   <<=
+
+3. Comparison (Relational) Operators
+
+    ==   !=   >   <   >=   <=
+
+4. Logical Operators
+
+    and   or   not
+
+5. Bitwise Operators
+
+    &   |   ^   ~   <<   >>
+
+6. Membership Operators
+
+    in   not in
+
+7. Identity Operators
+
+    is   is not
+'''
