71f222ddcdf3c18f765aab196a5371e19eb1186d
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-21 20:33:03 +0530

    Add files via upload

---
+++ Practice Codes/07_operator_precedenec.py
@@ -0,0 +1,48 @@
+# operator_precedence.py
+
+# Highest → Lowest Precedence
+
+# 1. Parentheses
+# ()
+
+# 2. Exponent
+# **
+
+# 3. Unary operators
+# +   -   ~
+
+# 4. Multiplicative
+# *   /   //   %
+
+# 5. Additive
+# +   -
+
+# 6. Bitwise Shift
+# <<   >>
+
+# 7. Bitwise AND
+# &
+
+# 8. Bitwise XOR
+# ^
+
+# 9. Bitwise OR
+# |
+
+# 10. Comparison, Membership, Identity
+# <   <=   >   >=   !=   ==
+# in   not in
+# is   is not
+
+# 11. Logical NOT
+# not
+
+# 12. Logical AND
+# and
+
+# 13. Logical OR
+# or
+
+# 14. Assignment operators (lowest)
+# =   +=   -=   *=   /=   //=   %=   **=
+# &=   |=   ^=   >>=   <<=
