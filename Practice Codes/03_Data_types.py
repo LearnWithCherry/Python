da9d320cc9544409fb035db8e88afbefa6103432
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-21 20:32:31 +0530

    Add files via upload

---
+++ Practice Codes/03_Data_types.py
@@ -0,0 +1,18 @@
+name = "Rajat"
+age = 10
+gpa = 8.8
+isprime = True
+val = None
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(isprime))
+print(type(val))
+'''
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+<class 'NoneType'>
+
+'''
