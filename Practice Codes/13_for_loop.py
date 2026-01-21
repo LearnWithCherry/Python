c0f041e5fc90510a95500620d925afa7af3a15a0
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-21 20:35:28 +0530

    Add files via upload

---
+++ Practice Codes/13_for_loop.py
@@ -0,0 +1,30 @@
+# string = "Hello"
+
+# for v in string:
+#     print(v, end="")
+
+
+# a = int(input("\nEnter Number: "))
+
+# for i in range(a+1):
+#     print(i)
+
+# word = "Artificial Intelligence".lower()
+# count = 0
+# for i in word:
+#     if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+#         count = count + 1
+
+# print(f"Total number of I appear in the Word: {count}")
+# i = 1
+# for i in  range(i,11,2):
+#     print(i)
+
+
+n = 5
+x = 0
+i = 1
+for i in range(n+1):
+    x += i
+    # print(i)
+print(x)
